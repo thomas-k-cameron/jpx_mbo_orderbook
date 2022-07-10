7bb74df7203ef532caf5eca2133090ff0cffaeb0
Thomas Cameron <thomas.k.cameron.1218@gmail.com>
2022-07-10 13:56:40 +0000

    add: from record batch to MessageEnum

--- src/datatypes/message_enum.rs
+++ src/datatypes/message_enum.rs
@@ -2,6 +2,9 @@ use crate::{
     CombinationProduct, DeleteOrder, EquilibriumPrice, Executed, ExecutionWithPriceInfo, LegPrice,
     ProductInfo, PutOrder, SecondTag, SystemEventInfo, TickSize, TradingStatusInfo,
 };
+use crate::from_record_batch::*;
+use chrono::NaiveDateTime;
+use datafusion::arrow::record_batch::RecordBatch;
 
 macro_rules! dclr_message_enum {
     ($($ident:ident,)*) => {
@@ -14,6 +17,36 @@ macro_rules! dclr_message_enum {
                 match self {
                     $( MessageEnum::$ident(_) => $ident::TAG, )*
                 }
+            }
+
+            pub fn timestamp(&self) -> NaiveDateTime {
+                match self {
+                    $( MessageEnum::$ident(x) => x.timestamp, )*
+                }
+            }
+        }
+
+        impl FromRecordBatch for MessageEnum {
+            fn validate(rb: &RecordBatch) -> bool {
+                $(
+                    let $ident = $ident::validate(rb);
+                )*
+                $(
+                    $ident &&
+                ) * true
+            }
+            fn from_record_batch(rb: &RecordBatch) -> Result<Vec<Self>, Vec<FromRecordBatchError>> {
+                $(
+                    let $ident = $ident::validate(rb);
+                    if $ident {
+                        return match $ident::from_record_batch(rb) {
+                            Ok(i) => Ok(i.into_iter().map(|i| MessageEnum::$ident(i)).collect()),
+                            Err(e) => Err(e)
+                        }
+                    }
+                )*
+
+                Err(vec![])
             }
         }
 
