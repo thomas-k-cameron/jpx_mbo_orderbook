b0a0415cd13c6b664a70379465875da5948f5266
Thomas Cameron <thomas.k.cameron.1218@gmail.com>
2022-11-23 03:18:36 +0000

    replace expect with .unwrap_or_else

--- src/orderbook/callback_datatype.rs
+++ src/orderbook/callback_datatype.rs
@@ -8,18 +8,22 @@ pub struct OrderExecution {
     pub msg: Executed,
 }
 
-
 #[derive(Debug, Clone)]
 pub struct CTagWithCorrespondingPTag {
     pub combo_group_id: i64,
     pub c_tag: Vec<ExecutionWithPriceInfo>,
     pub matched_add_order: Vec<AddOrder>,
-    pub p_tags: Vec<LegPrice>
+    pub p_tags: Vec<LegPrice>,
 }
 impl CTagWithCorrespondingPTag {
     pub fn order_book_id(&self) -> i64 {
-        self.c_tag.iter().next().expect(&format!("{self:#?}")).order_book_id
+        self.c_tag
+            .iter()
+            .next()
+            .unwrap_or_else(|| panic!("{self:#?}"))
+            .order_book_id
     }
+
     pub fn qty_by_order_book_id(&self) -> HashMap<i64, i64> {
         let mut map = HashMap::new();
         for i in self.c_tag.iter() {
@@ -27,23 +31,37 @@ impl CTagWithCorrespondingPTag {
         }
         map
     }
+
     pub fn executed_quantity(&self) -> i64 {
         if cfg!(test) {
             self._test();
         }
         if self.c_tag.len() > 1 {
-            self.c_tag.iter().filter(|i| i.side == Side::Buy).fold(0, |a, b| a + b.executed_quantity)
+            self.c_tag
+                .iter()
+                .filter(|i| i.side == Side::Buy)
+                .fold(0, |a, b| a + b.executed_quantity)
         } else {
             self.c_tag[0].executed_quantity
         }
     }
+
     pub fn occured_at_cross(&self) -> bool {
         // this function is not expected to be called when there is no item in `c_tag` field
         self.c_tag[0].occurred_at_cross
     }
+
     fn _test(&self) {
-        let qty = self.c_tag.iter().filter(|i| i.side == Side::Buy).fold(0, |a, b| a + b.executed_quantity);
-        let qty2 = self.c_tag.iter().filter(|i| i.side == Side::Sell).fold(0, |a, b| a + b.executed_quantity);
+        let qty = self
+            .c_tag
+            .iter()
+            .filter(|i| i.side == Side::Buy)
+            .fold(0, |a, b| a + b.executed_quantity);
+        let qty2 = self
+            .c_tag
+            .iter()
+            .filter(|i| i.side == Side::Sell)
+            .fold(0, |a, b| a + b.executed_quantity);
         if self.c_tag.len() > 1 {
             assert_eq!(qty, qty2);
         };
