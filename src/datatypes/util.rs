b1578ef724a23d692c2dc0eda99d3833da47cef0
Thomas K Cameron <thomas.k.cameron.1218@gmail.com>
2023-12-03 22:55:18 +0900

    update

--- src/datatypes/util.rs
+++ src/datatypes/util.rs
@@ -5,6 +5,8 @@ use std::str::{
 
 use chrono::NaiveDateTime;
 
+use crate::OrderBook;
+
 pub fn extract_value<'a>(s: &'a str) -> Option<&'a str> {
     if let Some((a, b)) = s.find("(").zip(s.find(")")) {
         return Some(&s[a + 1..b]);
@@ -34,6 +36,56 @@ pub fn extract_datetime<'a>(s: &'a str) -> Option<NaiveDateTime> {
         return NaiveDateTime::parse_from_str(&s[..a], "%Y-%m-%dT%H:%M:%S%.9f").ok();
     }
     None
+}
+
+pub fn ticks_between_price(book: &OrderBook, price_1: i64, price_2: i64) -> Option<i64> {
+    if price_1 == price_2 {
+        return Some(0);
+    }
+
+    let mut arr = [price_1, price_2];
+    arr.sort();
+
+    let mut rev_tick = (None, None);
+    for i in book.tick_info.iter() {
+        let r = i.price_from..i.price_to;
+        if r.contains(&arr[0]) {
+            rev_tick.0.replace(i);
+        }
+        if r.contains(&arr[1]) {
+            rev_tick.1.replace(i);
+        }
+    }
+
+    let (a, b) = rev_tick;
+    match (a, b) {
+        (Some(a), Some(b)) => {
+            if a.price_from == b.price_from && a.price_to == b.price_to {
+                return Some((arr[1] - arr[0]) / a.tick_size);
+            } else {
+                let lower_range = a.price_to - arr[0];
+                let upper_range = arr[1] - b.price_from;
+                let count1 = lower_range / a.tick_size;
+                let count2 = upper_range / a.tick_size;
+                return Some(count1 + count2);
+            }
+        }
+        _ => return None,
+    }
+}
+
+pub fn is_out_of_tick_range(book: &OrderBook, price: i64) -> bool {
+    let mut min = i64::MAX;
+    let mut max = i64::MIN;
+    for i in book.tick_info.iter() {
+        min = if min > i.price_from {
+            i.price_from
+        } else {
+            min
+        };
+        max = if max < i.price_to { i.price_to } else { max };
+    }
+    (min..=max).contains(&price)
 }
 
 ///
